a152c7a1628d8c635c14996f08e7e58c8482b47e
lakshya aggarwal <lakshya.aggarwal.007@gmail.com>
2021-05-16 21:37:33 +0530

    May Challenge 2021 Division 3 (Rated) LKDNGOLF

--- Codechef.java
+++ Codechef.java
@@ -1,21 +1,66 @@
-/* package codechef; // don't place package name! */
 
+/* package codechef; // don't place package name! */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		Scanner scanner = new Scanner(System.in);
+class Codechef {
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	public static void main(String[] args) {
+		FastReader scanner = new FastReader();
 		int testCases = scanner.nextInt();
-		for(int i = 0; i < testCases; i++){
-			int X = scanner.nextInt();
-			int A = scanner.nextInt();
-			int B = scanner.nextInt();
-			System.out.println((A + (100-X)*B)*10);
+		for (int i = 0; i < testCases; i++) {
+			int N = scanner.nextInt();
+			int x = scanner.nextInt();
+			int k = scanner.nextInt();
+			if ((x % k == 0 && x / k < N + 2) || ((N - x + 1) % k == 0 && (N - x + 1) / k < N + 2))
+				System.out.println("YES");
+			else
+				System.out.println("NO");
 		}
 	}
 }
